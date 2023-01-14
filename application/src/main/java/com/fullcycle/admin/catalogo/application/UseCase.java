c5f4cca84884639b60bed01534e44f5be7b9c8f2
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2023-01-14 16:35:04 -0300

    criando a entidade category com tdd

--- application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.fullcycle.admin.catalogo.application;
 
-import com.fullcycle.admin.catalogo.domain.Category;
-
 public class UseCase {
     public Category execute() {
         return new Category();
