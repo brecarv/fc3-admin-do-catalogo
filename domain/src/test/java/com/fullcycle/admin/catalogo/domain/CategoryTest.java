ee77530220c3206031daba677a60ee9b993e6d89
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2023-01-14 15:48:16 -0300

    configurando submodulos do projeto base

---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/CategoryTest.java
@@ -0,0 +1,11 @@
+package com.fullcycle.admin.catalogo.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+    @Test
+    public void testNewCategory() {
+        Assertions.assertNotNull(new Category());
+    }
+}
