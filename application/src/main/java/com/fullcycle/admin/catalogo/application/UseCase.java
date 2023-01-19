b1de89f45a12a1d9c203d5eb1140f77e01b1c3d8
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2023-01-19 14:32:08 -0300

    Estrutura base do Usecase

--- application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.fullcycle.admin.catalogo.application;
 
-public class UseCase {
-    public Category execute() {
-        return new Category();
-    }
+public abstract class UseCase<IN, OUT> {
+    public abstract OUT execute(IN anIn);
 }
